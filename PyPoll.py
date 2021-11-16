35f2be785c275d02ae6a4606f156a4b05cf5b497
Yurii Hanley <yuriihanley@Yuriis-MacBook-Pro.local>
2021-11-15 21:57:12 -0800

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,22 +1,54 @@
 # The dat we need to retrieve
 # Open the election results and read the file.
-# Add our dependencies
 
+# Add our dependencies.
 import csv
 import os
-
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Initialze variables
+total_votes = 0
+cadidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+        candiate_name = row[2]
+        if candiate_name not in cadidate_options:
+            cadidate_options.append(candiate_name)
+            candidate_votes[candiate_name] = 0
+        candidate_votes[candiate_name] +=1
+        
+    
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes)/float(total_votes)*100
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
 
 # A complete list of candidates who recieved votes
 # The percentage of votes each candidate won
---
+++ python_practice.py
@@ -0,0 +1,10 @@
+print("Hello World")
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+    
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
